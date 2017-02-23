8515941c265cf752d140be8943614ed97c1532c7
Carter Jiang <yejiangholy@gmail.com>
2017-02-23 15:35:40 -0500

    some practice files

---
+++ B3_GlobalEnvironment/Starter/app.js
@@ -0,0 +1,18 @@
+b();
+console.log(a);
+
+var a = 'Hello World!';
+
+function b() {
+    console.log('called b here !');
+}
+
+var person = new Object();
+var person = {firstName:'Carter',
+              lastName:'Alicea',
+              address: {
+                  street: '111 Main St',
+                  city: ' New York',
+                  state: 'NY'
+              }
+             };
