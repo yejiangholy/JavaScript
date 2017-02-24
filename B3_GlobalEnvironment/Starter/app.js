1745ec493f2d4029a4414ef4057af2b59ca9c122
Carter Jiang <yejiangholy@gmail.com>
2017-02-24 10:03:18 -0500

    funtion are objects !

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -12,6 +12,15 @@ var objectLiteral = {
 // from object --> Json string formart 
 JSON.stringify(english)
 
-console.log(JSON.stringify(english))
+//console.log(JSON.stringify(english))
 
 // from Json String --> to an object 
+
+function greet(){
+    console.log('Hi');
+}
+
+greet.language = 'english';
+
+greet();
+console.log(greet.language);
