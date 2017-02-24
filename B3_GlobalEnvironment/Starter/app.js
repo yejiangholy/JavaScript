8ade18780d1682bc160aeded420b212a2abd9620
Carter Jiang <yejiangholy@gmail.com>
2017-02-24 10:17:18 -0500

    assign & play with funciton(Object)

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -16,6 +16,9 @@ JSON.stringify(english)
 
 // from Json String --> to an object 
 
+
+
+// funciton is object !!! 
 function greet(){
     console.log('Hi');
 }
@@ -24,3 +27,9 @@ greet.language = 'english';
 
 greet();
 console.log(greet.language);
+
+var anno = function() {
+    console.log('I am a anno function');
+}
+
+anno();
